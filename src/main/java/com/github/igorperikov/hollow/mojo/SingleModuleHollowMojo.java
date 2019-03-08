2ee3900d77666011886fb79a04bf5279c4a277cf
Igor Perikov <IgorPerikov@users.noreply.github.com>
2019-03-08 18:48:36 +0300

    Update SingleModuleHollowMojo.java

--- src/main/java/com/github/igorperikov/hollow/mojo/SingleModuleHollowMojo.java
+++ src/main/java/com/github/igorperikov/hollow/mojo/SingleModuleHollowMojo.java
@@ -33,7 +33,7 @@ public class SingleModuleHollowMojo extends AbstractHollowMojo {
                         useErgonomicShortcuts,
                         usePackageGrouping,
                         restrictApiToFieldType,
-						classPostfix
+                        classPostfix
                 )
         );
 
